7850a43c16e550b9f518e79ef85895e99beb7a9f
jiabhullar <jiakbhullar@gmail.com>
2025-10-23 20:57:28 +0100

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() {
-   runApp(const App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({super.key});
 
-@override
-Widget build(BuildContext context) {
-  return MaterialApp(
-    title: 'Sandwich Shop App',
-    home: Scaffold(
-      appBar: AppBar(title: const Text('Sandwich Counter')),
-      body: const Center(
-        child: OrderItemDisplay(5, 'Footlong'),
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sandwich Shop App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sandwich Counter')),
+        body: Center(
+          child: Container(
+            width: 250,       // fixed width
+            height: 100,      // fixed height
+            color: Colors.blue, // visible background color
+            child: const OrderItemDisplay(5, 'Footlong'),
+          ),
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
 
 class OrderItemDisplay extends StatelessWidget {
@@ -27,9 +32,8 @@ class OrderItemDisplay extends StatelessWidget {
 
   const OrderItemDisplay(this.quantity, this.itemType, {super.key});
 
-@override
-Widget build(BuildContext context) {
-  return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
-}
-
+  @override
+  Widget build(BuildContext context) {
+    return Text('$quantity $itemType sandwich(es): ${'🥪' * quantity}');
+  }
 }
