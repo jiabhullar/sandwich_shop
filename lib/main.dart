e01e96500f24ea4e1f85c34944f2d373e02ab9c9
jiabhullar <jiakbhullar@gmail.com>
2025-11-13 18:39:49 +0000

    Add add and remove buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,31 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sandwich Shop App',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Sandwich Counter')),
-        body: const Center(
-          child: OrderItemDisplay(5, 'Footlong'),
+    home: Scaffold(
+      appBar: AppBar(title: const Text('Sandwich Counter')),
+  // The bit that you need to update starts from here
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const OrderItemDisplay(5, 'Footlong'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => print('Add button pressed!'),
+                  child: const Text('Add'),
+                ),
+                ElevatedButton(
+                  onPressed: () => print('Remove button pressed!'),
+                  child: const Text('Remove'),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+    ),
     );
   }
 }
