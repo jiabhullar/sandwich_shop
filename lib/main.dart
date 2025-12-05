df4e1895c409a26f3841964f733ebc0691b4dc2f
jiabhullar <jiakbhullar@gmail.com>
2025-12-05 08:18:20 +0000

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,18 @@
 import 'package:flutter/material.dart';
-import 'models/cart.dart';
-import 'screens/order_screen.dart';
-import 'screens/cart_screen.dart';
-import 'screens/checkout_screen.dart';
-import 'screens/app_styles.dart';
+import 'package:sandwich_shop/views/order_screen.dart';
 
 void main() {
-  runApp(const SandwichShopApp());
+  runApp(const App());
 }
 
-class SandwichShopApp extends StatelessWidget {
-  const SandwichShopApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Sandwich Shop',
-      theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => OrderScreen(cart: Cart()),
-        '/cart': (context) => CartScreen(),
-        '/checkout': (context) => CheckoutScreen(),
-        '/about': (context) => AboutScreen(),
-      },
+    return const MaterialApp(
+      title: 'Sandwich Shop App',
+      home: OrderScreen(maxQuantity: 5),
     );
   }
 }
